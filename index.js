f808ab7502ccf5b67126e510f297a610bcae6214
redigomdp1181-glitch <redigomdp1181@gmail.com>
2026-06-10 09:35:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,19 @@ const chatHistories = {};
 let botStatus = 'desconectado';
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
     puppeteer: {
-    headless: true,
-    args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-    ]
-}
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // A veces esto ayuda mucho en servidores
+            '--disable-gpu'
+        ]
+    }
 });
 
 client.on('qr', (qr) => {
